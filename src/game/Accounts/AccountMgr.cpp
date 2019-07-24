9f2a0c1d8a7b94faf15a3563f801170c2d2c87fe
Samet Luigis <sametlol123@hotmail.com>
2019-07-24 16:17:47 +0800

    Revert "fix account create command handler with expansion"
    This reverts commit 8b78fe7e68c66af108ca13f72404a13b23d81495.

--- src/game/Accounts/AccountMgr.cpp
+++ src/game/Accounts/AccountMgr.cpp
@@ -68,20 +68,7 @@ AccountOpResult AccountMgr::CreateAccount(std::string username, std::string pass
         return AOR_NAME_ALREADY_EXIST;                       // username does already exist
     }
 
-    SRP6 srp;
-
-    srp.CalculateVerifier(CalculateShaPassHash(username, password));
-    const char* s_hex = srp.GetSalt().AsHexStr();
-    const char* v_hex = srp.GetVerifier().AsHexStr();
-
-    bool update_sv = LoginDatabase.PExecute(
-        "INSERT INTO account(username,v,s,joindate,expansion) VALUES('%s','%s','%s',NOW(), %u)",
-            username.c_str(), v_hex, s_hex, expansion);
-
-    OPENSSL_free((void*)s_hex);
-    OPENSSL_free((void*)v_hex);
-
-    if (!update_sv)
+    if (!LoginDatabase.PExecute("INSERT INTO account(username,sha_pass_hash,joindate,expansion) VALUES('%s','%s',NOW(),'%u')", username.c_str(), CalculateShaPassHash(username, password).c_str(), expansion))
         return AOR_DB_INTERNAL_ERROR;                       // unexpected error
     LoginDatabase.Execute("INSERT INTO realmcharacters (realmid, acctid, numchars) SELECT realmlist.id, account.id, 0 FROM realmlist,account LEFT JOIN realmcharacters ON acctid=account.id WHERE acctid IS NULL");
 

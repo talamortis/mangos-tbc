0f123c0ed75b44d3cc48ea9459c537f314fa6fd9
coolzoom <tommingc@gmail.com>
2020-07-16 20:19:53 +0800

    Update example_gossip_codebox.cpp

--- src/game/AI/ScriptDevAI/scripts/examples/example_gossip_codebox.cpp
+++ src/game/AI/ScriptDevAI/scripts/examples/example_gossip_codebox.cpp
@@ -38,36 +38,41 @@ enum
 #define GOSSIP_ITEM_1       "A quiz: what's your name?"
 #define GOSSIP_ITEM_2       "I'm not interested"
 
-// This function is called when the player opens the gossip menubool
-bool GossipHello_example_gossip_codebox(Player* pPlayer, Creature* pCreature)
+
+class example_gossip_codebox : public CreatureScript
 {
-    pPlayer->ADD_GOSSIP_ITEM_EXTENDED(0, GOSSIP_ITEM_1, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1, "", 0, true);
-    pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_ITEM_2, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 2);
-
-    pPlayer->PlayerTalkClass->SendGossipMenu(907, pCreature->GetObjectGuid());
-
-    return true;
-}
-
-// This function is called when the player clicks an option on the gossip menubool
-bool GossipSelect_example_gossip_codebox(Player* pPlayer, Creature* pCreature, uint32 /*uiSender*/, uint32 uiAction)
-{
-    if (uiAction == GOSSIP_ACTION_INFO_DEF + 2)
+public:
+    example_gossip_codebox() : CreatureScript("example_gossip_codebox") { }
+    // This function is called when the player opens the gossip menubool
+    bool OnGossipHello(Player* pPlayer, Creature* pCreature) override
     {
-        DoScriptText(SAY_NOT_INTERESTED, pCreature);
-        pPlayer->CLOSE_GOSSIP_MENU();
+        pPlayer->ADD_GOSSIP_ITEM_EXTENDED(0, GOSSIP_ITEM_1, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1, "", 0, true);
+        pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_ITEM_2, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 2);
+
+        pPlayer->PlayerTalkClass->SendGossipMenu(907, pCreature->GetObjectGuid());
+
+        return true;
     }
 
-    return true;
-}
-
-bool GossipSelectWithCode_example_gossip_codebox(Player* pPlayer, Creature* pCreature, uint32 uiSender, uint32 uiAction, const char* sCode)
-{
-    if (uiSender == GOSSIP_SENDER_MAIN)
+    // This function is called when the player clicks an option on the gossip menubool
+    bool OnGossipSelect(Player* pPlayer, Creature* pCreature, uint32 /*uiSender*/, uint32 uiAction) override
     {
-        switch (uiAction)
+        if (uiAction == GOSSIP_ACTION_INFO_DEF + 2)
         {
-            case GOSSIP_ACTION_INFO_DEF+1:
+            DoScriptText(SAY_NOT_INTERESTED, pCreature);
+            pPlayer->CLOSE_GOSSIP_MENU();
+        }
+
+        return true;
+    }
+
+    bool OnGossipSelectCode(Player* pPlayer, Creature* pCreature, uint32 uiSender, uint32 uiAction, const char* sCode) override
+    {
+        if (uiSender == GOSSIP_SENDER_MAIN)
+        {
+            switch (uiAction)
+            {
+            case GOSSIP_ACTION_INFO_DEF + 1:
                 if (std::strcmp(sCode, pPlayer->GetName()) != 0)
                 {
                     DoScriptText(SAY_WRONG, pCreature);
@@ -81,18 +86,15 @@ bool GossipSelectWithCode_example_gossip_codebox(Player* pPlayer, Creature* pCre
                 pPlayer->CLOSE_GOSSIP_MENU();
 
                 return true;
+            }
         }
+
+        return false;
     }
 
-    return false;
-}
+};
 
 void AddSC_example_gossip_codebox()
 {
-    Script* pNewScript = new Script;
-    pNewScript->Name = "example_gossip_codebox";
-    pNewScript->pGossipHello = &GossipHello_example_gossip_codebox;
-    pNewScript->pGossipSelect = &GossipSelect_example_gossip_codebox;
-    pNewScript->pGossipSelectWithCode = &GossipSelectWithCode_example_gossip_codebox;
-    pNewScript->RegisterSelf(false);
+    new example_gossip_codebox();
 }

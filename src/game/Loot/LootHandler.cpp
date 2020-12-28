79258aa4918409a5a8696b6c3834e58389a58f9c
coolzoom <tommingc@Gmail.com>
2020-12-28 22:37:01 +0800

    Adding null check for item loot

--- src/game/Loot/LootHandler.cpp
+++ src/game/Loot/LootHandler.cpp
@@ -70,8 +70,8 @@ void WorldSession::HandleAutostoreLootItemOpcode(WorldPacket& recv_data)
         if (item)
             item->SetLootState(ITEM_LOOT_CHANGED);
     }
-
-    sScriptDevMgr.OnLootItem(_player, item, lootItem->count, item->GetGUIDLow());
+    if (item)
+        sScriptDevMgr.OnLootItem(_player, item, lootItem->count, item->GetGUIDLow());
 }
 
 void WorldSession::HandleLootMoneyOpcode(WorldPacket& /*recv_data*/)

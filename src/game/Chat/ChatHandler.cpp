6528be00e55ed7e8b101c7a3ecac3646797bc92a
talamortis <talamortis@gmail.com>
2021-04-09 21:29:36 +0100

    [Core/Hook] OnEmote Hook

--- src/game/Chat/ChatHandler.cpp
+++ src/game/Chat/ChatHandler.cpp
@@ -586,6 +586,8 @@ void WorldSession::HandleEmoteOpcode(WorldPacket& recv_data)
     uint32 emote;
     recv_data >> emote;
 
+    sScriptDevMgr.OnPlayerEmote(GetPlayer(), emote);
+
     // restrict to the only emotes hardcoded in client
     if (emote != EMOTE_ONESHOT_NONE && emote != EMOTE_ONESHOT_WAVE)
         return;
